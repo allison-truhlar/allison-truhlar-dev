d7ad6506b5107eb7208b5777b85c195d6bc49b81
allison-truhlar <amtruhlar@gmail.com>
2024-08-18 15:23:14 -0400

    chore: add base url for GH pages

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -2,17 +2,19 @@ import rss from "@astrojs/rss";
 import { SITE_TITLE, SITE_DESCRIPTION } from "../config";
 import { getCollection } from "astro:content";
 
+const base = import.meta.env.BASE_URL;
+
 export async function get(context) {
-  const blog = await getCollection("blog");
+  const notes = await getCollection("notes");
   return rss({
     title: SITE_TITLE,
     description: SITE_DESCRIPTION,
     site: import.meta.env.SITE,
-    items: blog.map((post) => ({
+    items: notes.map((post) => ({
       title: post.data.title,
       pubDate: post.data.pubDate,
       description: post.data.description,
-      link: `/blog/${post.slug}/`,
+      link: `${base}/notes/${post.slug}/`,
     })),
   });
 }
