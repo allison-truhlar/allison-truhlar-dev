278734b04a64aa92de0d4b1bb0e8623d5800bab4
allison-truhlar <amtruhlar@gmail.com>
2024-08-19 07:13:27 -0400

    chore: adds base to more links; changes blog -> notes

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -4,12 +4,12 @@ import { getCollection } from "astro:content";
 
 const base = import.meta.env.BASE_URL;
 
-export async function get(context) {
+export async function GET() {
   const notes = await getCollection("notes");
   return rss({
     title: SITE_TITLE,
     description: SITE_DESCRIPTION,
-    site: import.meta.env.SITE,
+    site: import.meta.env.SITE + "/" + base,
     items: notes.map((post) => ({
       title: post.data.title,
       pubDate: post.data.pubDate,
